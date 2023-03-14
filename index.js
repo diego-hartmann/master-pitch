5d8d0d280acfbc5a6b5d48450df883c5d5156548
diego-hartmann <dtghartmann@gmail.com>
2023-03-14 00:07:02 -0300

    small changes

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const context = new AudioContext();
 
-export async function load( url  ){
+export async function load(url){
     return fetch(url)
-    .then(response => response.arrayBuffer())
-    .then(buffer => context.decodeAudioData(buffer));
+    .then(res => res.arrayBuffer())
+    .then(buff => context.decodeAudioData(buff));
 }
 
-export async function play( sample , sampleNote , noteToPlay ) {
-    const source = context.createBufferSource();
-    source.buffer = sample;
-    source.playbackRate.value = 2 ** ((noteToPlay - sampleNote) / 12);
-    source.connect(context.destination);
-    source.start(0);
+export async function play(sample, sampleNote, noteToPlay) {
+    const src = context.createBuffersrc();
+    src.buffer = sample;
+    src.playbackRate.value = 2 ** ( (noteToPlay - sampleNote) / 12 );
+    src.connect(context.destination);
+    src.start(0);
 }
