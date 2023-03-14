a2c0ca69683a379fa3a190d1bf4e1be2525753e5
diego-hartmann <dtghartmann@gmail.com>
2023-03-14 00:55:40 -0300

    fix: creteBufferSource()

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
+// https://github.com/diego-hartmann/master-pitch
 const context = new AudioContext();
-
 export async function load(url){
     return fetch(url)
     .then(res => res.arrayBuffer())
     .then(buff => context.decodeAudioData(buff));
 }
-
 export async function play(sample, sampleNote, noteToPlay) {
-    const src = context.createBuffersrc();
+    const src = context.createBufferSource();
     src.buffer = sample;
     src.playbackRate.value = 2 ** ( (noteToPlay - sampleNote) / 12 );
     src.connect(context.destination);
