ec92f6854dc60efecceab4c87ac12948a1d7f9df
diego-hartmann <dtghartmann@gmail.com>
2023-03-13 23:37:53 -0300

    converted to js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const context = new AudioContext();
 
-export async function load( url:string ){
+export async function load( url  ){
     return fetch(url)
     .then(response => response.arrayBuffer())
     .then(buffer => context.decodeAudioData(buffer));
 }
-export async function play( sample:AudioBuffer, sampleNote:number, noteToPlay:number ) {
+
+export async function play( sample , sampleNote , noteToPlay ) {
     const source = context.createBufferSource();
     source.buffer = sample;
     source.playbackRate.value = 2 ** ((noteToPlay - sampleNote) / 12);
